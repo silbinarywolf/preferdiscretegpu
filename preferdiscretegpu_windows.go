9cebc87387f845aa0f801e440697fffdf04d9d2e
Jae B <doogie1012@gmail.com>
2020-07-20 16:51:02 +1000

    break test on purpose test

--- preferdiscretegpu_windows.go
+++ preferdiscretegpu_windows.go
@@ -1,7 +1,7 @@
 package preferdiscretegpu
 
 /*
-#cgo LDFLAGS: -v -L${SRCDIR}/discretegpu_windows.def
+#cgo LDFLAGS: -L${SRCDIR}/preferdiscretegpu_windows.def
 
 __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
